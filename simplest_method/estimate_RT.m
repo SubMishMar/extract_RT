07348977dc962261ea8dbbaac9f5fe9519652f2f
SubMishMar <subodheenitr@gmail.com>
2018-07-23 11:37:40 -0700

    added noise to points

--- simplest_method/estimate_RT.m
+++ simplest_method/estimate_RT.m
@@ -8,16 +8,17 @@ Xi = a*cos(theta);
 Yi = b*sin(theta);
 Zi = zeros(size(Xi));
 XYZi = [Xi; Yi; Zi; ones(size(Xi))]; % q
-
-R = rotnx(75)*rotny(45)*rotnz(45);
-T = [2; 3; -1];
-
-disp('Actual Transformation');
-Trans = [R T; 0 0 0 1]
-XYZj = Trans*XYZi; 
 figure(1)
 plot3(XYZi(1, :), XYZi(2, :), XYZi(3, :));
 hold on;
+R = rotnx(75)*rotny(45)*rotnz(45);
+T = [2; 3; -1];
+
+XYZi = XYZi + 0.1*[randn(3, size(XYZi, 2));zeros(1, size(XYZi, 2))];
+disp('Actual Transformation');
+Trans = [R T; 0 0 0 1]
+XYZj = Trans*XYZi; 
+
 plot3(XYZj(1, :), XYZj(2, :), XYZj(3, :));
 hold off;
 grid;
